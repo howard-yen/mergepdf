944979ee0d2f0ceec802fa1bef0fc508387642eb
howard-yen <47925471+howard-yen@users.noreply.github.com>
2020-01-28 17:45:03 -0500

    Replace merger with reader and writer.

--- merge.py
+++ merge.py
@@ -1,16 +1,24 @@
 import os
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfFileMerger, PdfFileWriter, PdfFileReader
 import re
 
 def merge(dir):
-    merger = PdfFileMerger()
+    writer = PdfFileWriter()
     for file in os.listdir(dir):
         if(file.endswith('.pdf')):
-            with open(dir + "\\" + file, 'rb') as f:
-                merger.append(f)
+            print(str(file))
+            print(str(dir + "\\" + file))
+            reader = PdfFileReader(file)
+            for page in range(reader.getNumPages()):
+                writer.addPage(reader.getPage(page))
+
 
     with open(dir + "\\" + "mergedPDFs.pdf", "wb") as f:
-        merger.write(f)
+        writer.write(f)
 
 dir = input('Directory(empty for current): ')
+if(dir == ''):
+    dir = '.'
+
 merge(dir)
+print('---done---')
