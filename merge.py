9108ec787b974240c44271768ae9de38a01b7ffd
howard-yen <howardy2000@gmail.com>
2022-05-07 02:32:41 -0400

    update merge.py to use argparse instead of inputs

--- merge.py
+++ merge.py
@@ -1,6 +1,7 @@
 import os
 from PyPDF2 import PdfFileMerger, PdfFileWriter, PdfFileReader
 import re
+import argparse
 
 def merge(dir):
     merger = PdfFileMerger(strict=False)
@@ -9,17 +10,17 @@ def merge(dir):
     for file in files:
         if(file.endswith('.pdf')):
             print(str(file))
-            merger.append(os.path.join(dir, file))
+            merger.append(os.path.join(dir, file), import_bookmarks=False)
 
     with open(os.path.join(dir, "allLectures.pdf"), "wb") as f:
         merger.write(f)
 
 if __name__ == "__main__":
-    dir = input('Directory(empty for current): ')
-    if(dir == ''):
-        dir = os.getcwd()
-    while(not os.path.exists(dir)):
-        dir = input("Directory not found, please try again: ")
+    parser = argparse.ArgumentParser(description="Merge pdf files")
+    parser.add_argument("--input_dir", type=str, help="Directory containing all the pdf files")
+    args = parser.parse_args()
+    if not os.path.exists(args.input_dir):
+        raise Exception(f"input dir {args.input_dir} does not exist")
 
-    merge(dir)
+    merge(args.input_dir)
     print('---done---')
